3b9cd31fe9d62e5c9af57b0965273e0a8a27f46a
Damin3927 <d@damin.dev>
2026-01-11 17:06:12 +0900

    chore: Put back RandomChar

--- main.c
+++ main.c
@@ -1,45 +1,44 @@
+/* CFU Proving Ground since 2025-02    Copyright(c) 2025 Archlab. Science Tokyo /
+/ Released under the MIT license https://opensource.org/licenses/mit           */
+
+#include <stdlib.h>
 #include "atomic.h"
 #include "st7789.h"
+#include "perf.h"
 #include "util.h"
 
 #ifndef NCORES
 #define NCORES 4 // number of cores
 #endif
 
-#define INIT_COUNTER 1
-#define ITERATIONS 100
+volatile int count = 0;
+volatile spinlock_t lock = 0;
 
-volatile int shared_counter = INIT_COUNTER; // Shared counter
+void RandomChar() {
+    while (1) {
+        int x = rand() % 240;
+        int y = rand() % 240;
+        char c = 'A' + rand() % 26;
+        char color = rand() & 0x7;
 
-int main()
-{
+        spinlock_acquire(&lock);
+
+        count++;
+
+        pg_lcd_draw_char(x, y, c, color, 1);
+        pg_lcd_set_pos(0, 14);
+        pg_lcd_prints("steps :");
+        pg_lcd_printd(count);
+
+        spinlock_release(&lock);
+    }
+}
+
+int main () {
     int hart_id = pg_hart_id();
-
-    // Each core increments the shared counter ITERATIONS times
-    for (int i = 0; i < ITERATIONS; i++) {
-        atomic_fetch_add(&shared_counter, 1);
+    if (hart_id != 0) {
+        pg_lcd_reset();
     }
-
-    pg_barrier();
-
-    if (hart_id == 0) {
-        // Display the result
-        pg_lcd_set_pos(0, 0);
-        pg_lcd_prints("LR/SC Test");
-        pg_lcd_set_pos(0, 1);
-        pg_lcd_prints("Counter:");
-        pg_lcd_set_pos(0, 2);
-        pg_lcd_printd(shared_counter);
-        pg_lcd_set_pos(0, 3);
-
-        if (shared_counter == NCORES * ITERATIONS + INIT_COUNTER) {
-            pg_lcd_prints("PASS");
-        } else {
-            pg_lcd_prints("FAIL");
-        }
-    }
-
-    pg_barrier();
-
+    RandomChar();
     return 0;
 }
